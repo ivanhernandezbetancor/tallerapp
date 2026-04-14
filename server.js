12858806cb2c60c83dd70ff827d8827fb8387152
Ivan <ivanhernandezbetancor@gmail.com>
2026-04-14 12:30:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('public'));
-
+app.listen(3000);
 // Rutas API
 const clientesRoutes = require('./routes/clientes');
 app.use('/api/clientes', clientesRoutes);
